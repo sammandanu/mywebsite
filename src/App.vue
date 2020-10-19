173ea72947bf7016a4e6ce4687855ddf646f8651
sammandanu <sammandanu@gmail.com>
2020-10-19 17:25:45 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <div class="">Hello World</div>
     <router-view />
   </div>
 </template>
