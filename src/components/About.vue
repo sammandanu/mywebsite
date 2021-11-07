9bb42dd099fe069957b5a4f6800a6ac20739e27b
sammandanu <sammandanu@gmail.com>
2021-11-07 14:35:49 +0700

    [Add]Portofolio

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,31 +4,31 @@
     id="about"
   >
     <div class="w-2/3 mx-auto md:w-full md:col-span-2 md:mb-0">
-      <div class="p-4 mb-8  md:mb-0 rounded-md border border-orange">
+      <div class="p-4 mb-8 md:mb-0 rounded-md border border-orange">
         <img
           src="@/assets/about.png"
-          class="rounded-md block md:w-full "
+          class="rounded-md block md:w-full"
           alt=""
         />
       </div>
     </div>
     <div class="md:col-span-3">
-      <h2 class="text-2xl text-center md:text-left  text-white mb-8">
+      <h2 class="text-2xl text-center md:text-left text-white mb-8">
         <span class="pb-1 border-b border-orange">About</span>
       </h2>
       <p class="text-gray text-sm md:text-lg text-center md:text-left mb-4">
-        Hi, My name is Muhammad Samman Almadanu, I am 23 years old, you can call
-        me Danu. I have more than 4 years of experience in this field. I love to
-        learn and developing myself for a better me. For my web project, I use
-        Vue, Strapi and Laravel. <br /><br />Yes I do Full Stack!
+        Hi, My name is Muhammad Samman Almadanu, I am
+        {{ date.getFullYear() - 1997 }} years old, you can call me Danu. I have
+        more than {{ date.getFullYear() - 2018 }} years of experience in this
+        field. I love to learn and developing myself for a better me. For my web
+        project, I use Vue, Nuxt, Strapi and Laravel. <br /><br />Yes I do Full
+        Stack!
       </p>
-      <div
-        class="flex items-center justify-center md:justify-start  text-white"
-      >
+      <div class="flex items-center justify-center md:justify-start text-white">
         <a
           :href="
             'https://api.whatsapp.com/send?phone=6282221940237&text=' +
-              'Hello Danu'.split(' ').join('%20')
+            'Hello Danu'.split(' ').join('%20')
           "
           target="_blank"
           class="px-4 py-2 bg-orange rounded-md font-medium"
@@ -41,7 +41,12 @@
 
 <script>
 export default {
-  name: "About"
+  name: "About",
+  data() {
+    return {
+      date: new Date(),
+    };
+  },
 };
 </script>
 
--- src/components/Portofolio.vue
+++ src/components/Portofolio.vue
@@ -1,51 +1,58 @@
 <template>
   <div class="px-4 pb-20 bg-dark-gray pt-20" id="portofolio">
     <div class="max-w-screen-xl mx-auto">
-      <h2 class="text-2xl text-center  text-white mb-8">
+      <h2 class="text-2xl text-center text-white mb-8">
         <span class="pb-1 border-b border-orange">Portofolio</span>
       </h2>
-      <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
-        <div class="">
-          <img
-            src="@/assets/portofolio/p1.png"
-            class="w-full rounded-md"
-            alt=""
-          />
-        </div>
-        <div class="">
-          <img
-            src="@/assets/portofolio/p2.png"
-            class="w-full rounded-md"
-            alt=""
-          />
-        </div>
-        <div class="">
-          <img
-            src="@/assets/portofolio/p3.png"
-            class="w-full rounded-md"
-            alt=""
-          />
-        </div>
-        <div class="">
-          <img
-            src="@/assets/portofolio/ig1.png"
-            class="w-full rounded-md"
-            alt=""
-          />
-        </div>
-        <div class="">
-          <img
-            src="@/assets/portofolio/ig2.png"
-            class="w-full rounded-md"
-            alt=""
-          />
-        </div>
-        <div class="">
-          <img
-            src="@/assets/portofolio/ig3.png"
-            class="w-full rounded-md"
-            alt=""
-          />
+      <div class="mx-auto mb-8 text-center">
+        <button
+          @click.prevent="changePortofolio('all')"
+          class="inline-block mr-4 px-4 py-2  rounded-md focus:outline-none"
+          :class="
+            type === 'all' ? 'bg-orange text-white' : 'bg-md-gray text-white'
+          "
+        >
+          All
+        </button>
+        <button
+          @click.prevent="changePortofolio('website')"
+          class="inline-block mr-4 px-4 py-2 bg-orange text-white rounded-md focus:outline-none"
+          :class="
+            type === 'website'
+              ? 'bg-orange text-white'
+              : 'bg-md-gray text-white'
+          "
+        >
+          Website
+        </button>
+        <button
+          @click.prevent="changePortofolio('design')"
+          class="inline-block px-4 py-2  rounded-md focus:outline-none"
+          :class="
+            type === 'design' ? 'bg-orange text-white' : 'bg-md-gray text-white'
+          "
+        >
+          Design
+        </button>
+      </div>
+      <div class="grid grid-cols-1 gap-4 md:grid-cols-3" v-if="show">
+        <div class="overflow-hidden" v-for="(por, index) in show" :key="index">
+          <div class="w-full h-full">
+            <a
+              class=""
+              :href="
+                por.link
+                  ? por.link
+                  : 'https://www.instagram.com/samm.thing/?hl=id'
+              "
+            >
+              <img
+                :src="por.img"
+                class="w-full h-full object-top object-cover rounded-md"
+                alt=""
+              />
+            </a>
+          </div>
         </div>
       </div>
     </div>
@@ -54,7 +61,81 @@
 
 <script>
 export default {
-  name: "Portofolio"
+  name: "Portofolio",
+  data() {
+    return {
+      type: "all",
+      portofolio: [
+        {
+          img: require("@/assets/portofolio/ig3.png"),
+          link: null,
+          type: "design",
+        },
+        {
+          img: require("@/assets/portofolio/semesta.png"),
+          link: "https://yogyasemesta.com/",
+          type: "website",
+        },
+        {
+          img: require("@/assets/portofolio/jelajah.png"),
+          link: "https://beta.jelajah.live/",
+          type: "website",
+        },
+        {
+          img: require("@/assets/portofolio/ig1.png"),
+          link: null,
+          type: "design",
+        },
+        {
+          img: require("@/assets/portofolio/scu.png"),
+          link: "https://www.scu-perkisemarang.com/",
+          type: "website",
+        },
+        {
+          img: require("@/assets/portofolio/neuro.png"),
+          link: "https://strongneurologi.com/",
+          type: "website",
+        },
+        {
+          img: require("@/assets/portofolio/ig2.png"),
+          link: null,
+          type: "design",
+        },
+        {
+          img: require("@/assets/portofolio/p1.png"),
+          link: "https://www.nabilatransport.com/",
+          type: "website",
+        },
+        {
+          img: require("@/assets/portofolio/p3.png"),
+          link: "https://sammandanu.github.io/movienight/",
+          type: "website",
+        },
+      ],
+      show: null,
+    };
+  },
+  mounted() {
+    this.changePortofolio("all");
+  },
+  methods: {
+    changePortofolio(data) {
+      if (data === "all") {
+        this.type = "all";
+        this.show = this.portofolio;
+      } else if (data === "website") {
+        this.type = "website";
+        this.show = this.portofolio.filter((res) => {
+          return res.type === "website";
+        });
+      } else {
+        this.type = "design";
+        this.show = this.portofolio.filter((res) => {
+          return res.type === "design";
+        });
+      }
+    },
+  },
 };
 </script>
 
